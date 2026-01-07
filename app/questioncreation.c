4551c16d26b1745dce90ee785a43fcde6569b454
Tudor Ganshaw <tudorganshaw@gmail.com>
2026-01-07 18:19:20 -0500

    Finished question creation for stats, updated for base experience

--- app/questioncreation.c
+++ app/questioncreation.c
@@ -2,18 +2,33 @@
 
 char * types[18] = {"fire", "water", "grass", "electric", "ghost", "poison", "ice", "dragon", "bug", "normal", "fighting", "flying", "ground", "rock", "psychic", "dark", "steel", "fairy"};
 char * stat_types[6] = {"hp", "attack", "defense", "special attack", "special defense", "speed"};
-char * qtypes[12] = {"name", "gen", "height (dm)", "weight (dg)", "type", "hp", "attack", "defense", "special attack", "special defense", "speed", "base experience"};
+char * qtypes[12] = {"name", "gen", "height (dm)", "weight (hg)", "type", "hp", "attack", "defense", "special attack", "special defense", "speed", "base experience"};
 
 int main(){
   time_t t;
+  srand((int) (unsigned) time(&t));
+
+  int rand_qtype = rand() % 12;
+  //rand_qtype = (rand() % 6) + 5;
+  //rand_qtype = 11;
+  if(rand_qtype == 0){rand_qtype = 1;}
+
+  int rand_dexnum = rand() % 1025;
+  if(rand_dexnum <= 0){rand_dexnum = 1;}
+
 
   char * answers[4];
   for(int i = 0; i < 4; i++)
   {
     answers[i] = calloc(15, 1);
   }
-  int ans = questioncreation((int) (unsigned) time(&t), answers);
-  printf("Ans Pos: %d\n", ans);
+
+  char pkmn_name[20];
+  getName(rand_dexnum, pkmn_name);
+  printf("What is %s's %s?\n", pkmn_name, qtypes[rand_qtype]);
+
+  int ans = questioncreation(rand_dexnum, rand_qtype, (int) (unsigned) time(&t), answers);
+  printf("Ans: %s at pos %d\n", answers[ans], ans);
   for(int i = 0; i < 4; i++)
   {
     printf("%s, ", answers[i]);
@@ -21,7 +36,7 @@ int main(){
   printf("\n");
 }
 
-int questioncreation(int seed, char ** answers){
+int questioncreation(int rand_dexnum, int rand_qtype, int seed, char ** answers){
 
   srand(seed);
   int rand_types = rand() % 18;
@@ -29,12 +44,10 @@ int questioncreation(int seed, char ** answers){
   // printf("TypeRand: %d\nStatRand: %d\n", rand_types, rand_stat);
   // printf("Type: %s\nStat: %s\n", types[rand_types], stat_types[rand_stat]);
 
-  int rand_qtype = rand() % 12;
-  if(rand_qtype == 0){rand_qtype = 1;}
+
   // printf("Rand Qtype: %d\nQuestion: %s\n", rand_qtype, qtypes[rand_qtype]);
 
-  int rand_dexnum = rand() % 1025;
-  if(rand_dexnum <= 0){rand_dexnum = 1;}
+
   // printf("Rand Dexnum: %d\n",rand_dexnum);
 
   char pkmn_file[15] = "data/";
@@ -57,8 +70,7 @@ int questioncreation(int seed, char ** answers){
     qdata[i] = strsep(&qdata_buff, ";");
   }
 
-  rand_qtype = 4;
-  printf("What is %s's %s?\nAns: %s\n", qdata[0], qtypes[rand_qtype], qdata[rand_qtype]);
+    //rand_qtype = (rand() % 6) + 5;
    // Turn everything above into a helper function
    // Add in an array that [dexnum, qtype]
 
@@ -94,7 +106,8 @@ int questioncreation(int seed, char ** answers){
       }
 
       break;
-    case 2: case 3:
+    //case 2: case 3: case 11:
+    default:
       int real_val;
       sscanf(qdata[rand_qtype], "%d", &real_val);
       // printf("%s-\n", double_str);
@@ -121,6 +134,14 @@ int questioncreation(int seed, char ** answers){
           temp_val *= rand_offset;
           if(temp_val == 0){temp_val += 1;}
           if(real_val < 10){temp_val += rand() % 10;}
+          if(rand_qtype != 2 && rand_qtype != 3)
+          {
+            if(real_val - temp_val > real_val / 2)
+            {
+              temp_val += rand() % (real_val / 3);
+              temp_val += real_val / 4;
+            }
+          }
           sprintf(temp_str, "%d", temp_val);
         }
         int index = i + ans_position;
@@ -188,13 +209,23 @@ int questioncreation(int seed, char ** answers){
       }
 
       break;
-    case 11:
-      printf("not default\n");
-      printf("%s\n", qtypes[rand_qtype]);
-      break;
-    default:
-      printf("%s\n", qtypes[rand_qtype]);
-      break;
+
+    // default:
+    //   int correct_stat;
+    //   sscanf(qdata[rand_qtype],"%d", &correct_stat);
+    //   strcpy(answers[ans_position],qdata[rand_qtype]);
+    //
+    //   int temp_stat = correct_stat;
+    //   char temp_stat_str[15];
+    //   sprintf(temp_stat_str,"%d",temp_stat);
+    //   // while(isIn(answers,temp_stat_str))
+    //   // {
+    //   //
+    //   //
+    //   // }
+    //
+    //   printf("%d\n",correct_stat);
+    //   break;
   }
 
   return ans_position;
@@ -212,4 +243,24 @@ int isIn(char ** arr, char * str)
     i++;
   }
   return final;
+}
+
+void getName(int dexnum, char * pkmn_name)
+{
+  char pkmn_file[15] = "data/";
+  char dexnum_str[10];
+  sprintf(dexnum_str, "%d.txt", dexnum);
+  strcat(pkmn_file,dexnum_str);
+
+  FILE * info_file = fopen(pkmn_file, "r");
+  char info_buff[100];
+  fgets(info_buff, 100, info_file);
+
+  char * qdata[12];
+  char * qdata_buff = info_buff;
+  for(int i = 0; i < 12; i++)
+  {
+    qdata[i] = strsep(&qdata_buff, ";");
+  }
+  strcpy(pkmn_name, qdata[0]);
 }
