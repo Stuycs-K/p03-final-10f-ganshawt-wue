c02c1a33729b8c7bece6db3bc01f6ac35674c554
Eviss W <ewu60@stuy.edu>
2026-01-08 00:40:06 -0500

    lobby and server/client changes to fit plan

--- app/client.c
+++ app/client.c
@@ -1,67 +1,60 @@
 #include "networking.h"
 
 void sigint_handler(int sig) {
-  //printf("Client exit");
   exit(0);
 }
 
-void clientLogic(int server_socket){
+void lobby(int server_socket){
   char buffer[BUFFER_SIZE];
-  //Ask for string
-  printf("I think it's ");
-  if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
-    //printf("\nClient exit\n");
-    close(server_socket);
-    kill(getpid(), EOF);
-    exit(0);
+  while (1) {
+    int bytes_read = read(server_socket, buffer, sizeof(buffer) - 1);
+    if (bytes_read <= 0) {
+      printf("Server disconnected.\n");
+      exit(0);
+    }
+    buffer[bytes_read] = '\0';
+    if (strstr(buffer, "Game starting")) {
+      printf("%s", buffer);
+      break;
+    }
+    printf("\r%s", buffer);
+    fflush(stdout);
   }
-  buffer[strcspn(buffer, "\n")] = '\0';
-  //Send input to server
-  int bytes_written = write(server_socket, buffer, strlen(buffer));
-  if (bytes_written < 0) {
-    //printf("write() error: %s\n", strerror(errno));
-    return;
-  }
-  //printf("Client sent string to server: %s\n", buffer);
-  int bytes_read = read(server_socket, buffer, sizeof(buffer) - 1);
-  if (bytes_read < 0) {
-    //printf("read() error: %s\n", strerror(errno));
-    exit(0);
-  }
-  if (bytes_read == 0) {
-    //printf("Server disconnected\n");
-    close(server_socket);
-    //exit(0);
-  }
-  buffer[bytes_read] = '\0';
 }
 
-int main(int argc, char *argv[] ) {
-  signal(SIGINT, sigint_handler);
-  static char IP[256] = "127.0.0.1";
-  if(argc > 1)  {
-    strncpy(IP, argv[1], sizeof(IP) - 1);
+void game(int server_socket) {
+  char buffer[BUFFER_SIZE];
+  while (1) {
+    //TRIVIA FUNCTION REPLACE LATER
+    int bytes_read = read(server_socket, buffer, sizeof(buffer) - 1);
+    if (bytes_read <= 0) {
+      printf("Server disconnected\n");
+      close(server_socket);
+      exit(0);
+    }
+    buffer[bytes_read] = '\0';
   }
-  else  {
+}
+
+int main(int argc, char *argv[]) {
+  signal(SIGINT, sigint_handler);
+  char IP[256] = "127.0.0.1";
+  
+  if (argc > 1) {
+    strncpy(IP, argv[1], sizeof(IP) - 1);
+  } else {
     FILE *fp = fopen(".server_ip", "r");
     if (fp) {
-      if (fscanf(fp, "%255s", IP) == 1) {
-        //printf("Connecting to server at: %s\n", IP);
-      }
-      else  {
+      if (fscanf(fp, "%255s", IP) != 1) {
         strcpy(IP, "127.0.0.1");
       }
       fclose(fp);
     }
-    else  {
-      //printf("No server ip found, using 127.0.0.1\n");
-    }
   }
   int server_socket = client_tcp_handshake(IP);
-  //printf("Client connected.\n");
-  while (1) {
-    clientLogic(server_socket);
-  }
+  printf("Connected to server\n");
+  lobby(server_socket);
+  game(server_socket);
   close(server_socket);
   return 0;
 }
